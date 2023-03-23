c01097dec5deee7d664aa7594b7a69a030f5f016
MsKesewaa <elsiebrian19@gmail.com>
2023-03-23 10:19:49 -0700

    1 main

--- 0x03-debugging/1-main.c
+++ 0x03-debugging/1-main.c
@@ -1,16 +1,24 @@
-#include "main.h"
 #include <stdio.h>
 
-/** 
- * kdj
+/**
+ * main - causes an infinite loop
+ * Return: 0
  */
 
-void positive_or_negative(int i)
+int main(void)
 {
-	if (i > 0)
-		printf("%d is positive\n", i);
-	else if (i < 0)
-		printf("%d is negative\n", i);
-	else
-		printf("%d is zero\n", i);
+	int i;
+
+	printf("Infinite loop incoming :(\n");
+
+	i = 0;
+
+	/* while (i < 10)*/
+	/* {*/
+	/*	putchar(i);*/
+	/* }*/
+
+	printf("Infinite loop avoided! \\o/\n");
+
+	return (0);
 }
